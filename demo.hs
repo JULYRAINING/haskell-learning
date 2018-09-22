2ffe5d21e823a3def50552f8783abe42c75f2f31
shibo <shibai@100tal.com>
2018-09-23 00:31:37 +0800

    first commit

---
+++ demo.hs
@@ -0,0 +1,18 @@
+doubleMe x = x + x
+--doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+     then x 
+     else x*2
+
+lostNumbers = [4, 8, 15, 16, 23, 48]
+
+list_a = [1, 2, 3, 4]
+list_b = [5, 6, 7, 8]
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length'  xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [c |c <- st, c `elem` ['A'..'Z']]
