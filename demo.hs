6231126879e966c34cc1737c2ef3cd510d1debfe
shibo <shibai@100tal.com>
2018-11-17 01:26:44 +0800

    add comment

--- demo.hs
+++ demo.hs
@@ -12,7 +12,7 @@ list_a = [1, 2, 3, 4]
 list_b = [5, 6, 7, 8]
 
 boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
-
+--求数组的长度
 length'  xs = sum [1 | _ <- xs]
-
+--删除除了大写字母在内的其他字符
 removeNonUppercase st = [c |c <- st, c `elem` ['A'..'Z']]
